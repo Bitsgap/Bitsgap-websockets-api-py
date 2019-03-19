f1649c096284f081cabcb6e7b00f5885114e32e3
Bitsgap <30630956+Bitsgap@users.noreply.github.com>
2019-03-19 10:13:03 +0300

    Update tests_demo_user_orders_cancel.py
    Changed cancel format

--- tests/tests_demo_user_orders_cancel.py
+++ tests/tests_demo_user_orders_cancel.py
@@ -14,18 +14,16 @@ class TestWSDemoOrdersCancel(TestCase):
 
     authClient.subscribe_logs()
 
-    market = 'bittrex'
-
-    demo_key = 'bittrex@63d9a386-d083-11e7-9ac6-901b0edee5f6'
+    market = 'bittrex.demo'
 
     authClient.subscribe_orders_open_demo(market)
 
     id = '282e68c79fd14c80ae1c67feb59691b2'
 
-    authClient.cancel_order_demo(demo_key, id)
+    authClient.cancel_order_demo(market, id)
 
     id = '2b63c7f0b91441d890f37c10dd582b21'
 
-    authClient.cancel_order_demo(demo_key, id)
+    authClient.cancel_order_demo(market, id)
 
     authClient.start(callback=my_handler)
